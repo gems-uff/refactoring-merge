cdb32151748cdc616ae083c3f3873d08916c817d
Pedro <leomurta@ic.uff.br>
2019-04-19 10:18:05 -0300

    Removes hardcoded paths and adds support for parallel execution

--- mergeeffort/run_repos.py
+++ mergeeffort/run_repos.py
@@ -1,10 +1,10 @@
-
 import csv
 import merge_analysis
+import sys
 
-def select_repos():
+def select_repos(input_file):
 	repos = []
-	with open('merges_count2.csv') as csv_file:
+	with open(input_file) as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		line_count = 0
 		for row in csv_reader:
@@ -13,15 +13,19 @@ def select_repos():
 				repos.append(row[0])
 				#run_script(path)
 
-		print('Processed ' + str(line_count) + ' repos')
+		print('Selected ' + str(line_count) + ' repos to analyze:', repos)
 	return repos
 			
 def main():
-	selected_repos = select_repos()
-	for repo in selected_repos:
-		print(repo)
-		merge_analysis.init_analysis(repo, False, True)
+	if len(sys.argv) != 3:
+		print('Syntax: python3 run_repos.py <input file> <output file>')
+	else:
+		input_file = sys.argv[1]
+		output_file = sys.argv[2]
+		selected_repos = select_repos(input_file)
+		for repo in selected_repos:
+			print(repo)
+			merge_analysis.init_analysis(repo, output_file, False, True)
 	
 if __name__ == '__main__':
-	main()	
-
+	main()
