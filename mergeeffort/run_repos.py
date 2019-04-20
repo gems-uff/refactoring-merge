50679a2c085d3ae513e264bb326244c55c4f2e14
Pedro <leomurta@ic.uff.br>
2019-04-20 09:02:27 -0300

    Using sequential id instead of pid to avoid collision

--- mergeeffort/run_repos.py
+++ mergeeffort/run_repos.py
@@ -18,31 +18,30 @@ def select_repos(input_file):
     return repos
 
 
-def run(repo, output_prefix, log_prefix):
-    pid = os.getpid()
-    output_file = os.getcwd() + os.sep + output_prefix + str(pid) + '.csv'
-    log_file = os.getcwd() + os.sep + log_prefix + str(pid) + '.txt'
+def run(repo, output_file, log_file):
+    output_path = os.getcwd() + os.sep + output_file
+    log_path = os.getcwd() + os.sep + log_file
 
-    print(f'Process {pid} started for project {repo}.\nSaving output in {output_file}.\nSaving log in {log_file}.')
-    with open(log_file, "w") as out:
-        subprocess.run(f'python3 merge_analysis.py --local {repo} --collect --output {output_file}',
+    print(f'Starting project {repo}.\n\tOutput: {output_file}.\n\tLog: {log_file}.')
+    with open(log_path, "w") as out:
+        subprocess.run(f'python3 merge_analysis.py --local {repo} --collect --output {output_path}',
                        shell=True, stdout=out, stderr=subprocess.STDOUT)
-    print(f'Process {pid} finished.')
+    print(f'Finished project {repo}.')
 
 def main():
     parser = argparse.ArgumentParser(description='Merge effort analysis over multiple projects')
     parser.add_argument("--processes", type=int, default=os.cpu_count(), help="number of processes to run in parallel. Default: number of cpus")
+    parser.add_argument("--output-prefix", default="output-", help="output prefix of the CSV file to be generated, containing the collected metrics. Default: 'output-'")
+    parser.add_argument("--log-prefix", default="log-", help="log file prefix. Default: 'log-'")
     parser.add_argument("input", help="input CSV file containing the path and the number of merges of each repository")
-    parser.add_argument("--output-prefix", default="output", help="output prefix of the CSV file to be generated, containing the collected metrics. Default: 'output'")
-    parser.add_argument("--log-prefix", default="log", help="log file prefix. Default: 'log'")
 
     args = parser.parse_args()
 
     selected_repos = select_repos(args.input)
 
     pool = Pool(processes=args.processes, maxtasksperchild=1)
-    for repo in selected_repos:
-        pool.apply_async(run, args=(repo, args.output_prefix, args.log_prefix))
+    for i, repo in enumerate(selected_repos):
+        pool.apply_async(run, args=(repo, args.output_prefix + str(i) + '.csv', args.log_prefix + str(i) + '.txt'))
     pool.close()
     pool.join()
 
