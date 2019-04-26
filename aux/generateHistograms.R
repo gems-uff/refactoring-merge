bbb9b6ed4b9211b856d491f2997808b84186686d
tayanemoura <tayanemoura@id.uff.br>
2019-04-25 22:23:20 -0300

    adds merge analyzed script

--- aux/generateHistograms.R
+++ aux/generateHistograms.R
@@ -1,6 +1,6 @@
 library(arules)
 
-project <- read.csv("~/Documents/git/merge-effort-mining/aux/finished-output.csv")
+project <- read.csv("~/Documents/git/merge-effort-mining/result/output.csv")
 #project_numeric <- project[sapply(project, is.numeric)]
 
 # loop over column *names* instead of actual columns
@@ -8,5 +8,12 @@ sapply(names(project), function(cname){
   # (make sure we only plot the numeric columns)
   if (is.numeric(project[[cname]]))
     # use the `main` param to put column name as plot title
-    hist(project[[cname]], main=cname, breaks=20)
+    #mean_project = mean(project[[cname]])
+    
+    # pode ser main = "Equal Frequency",  "Equal Interval length", "K-Means", 
+    hist(project[[cname]], main=cname, breaks=10)
+    abline(v = mean(project[[cname]]), col="red", lwd=3, lty=2)
 })
+
+
+
